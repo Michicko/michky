c3e775653d214ba111f57f6ad3aa70aa28842ac3
Michael Solomon <michicko81@gmail.com>
2022-04-14 11:26:18 +0100

    cleaned up and removed nav-menu

--- client/src/components/Navigation/Navigation.js
+++ client/src/components/Navigation/Navigation.js
@@ -1,30 +1,13 @@
-import { usePageContext } from "../../contexts/page_context";
-import { BiMenuAltRight } from 'react-icons/bi';
-import logo from '../../assets/images/logo.png';
-import NavigationMenu from "./NavigationMenu";
-
 const Navigation = () => {
-  const { is_nav_menu_opened } = usePageContext();
+	return (
+		<div className='navigation'>
+			<input type='checkbox' id='navi-toggle' className='navigation-checkbox' />
+			<label htmlFor='navi-toggle' className='navigation-btn'></label>
+			<div className='navigation-bg'>&nbsp;</div>
 
-  return (
-		<div className='navbar'>
-			<nav className='nav'>
-        {/* logo */}
-        <img src={logo} alt="Michky logo" className="nav-logo" />
-
-				{/* nav menu btn */}
-				{!is_nav_menu_opened && (
-					<button className='nav-btn' type='button'>
-						<BiMenuAltRight className='nav-icon' />
-					</button>
-				)}
-			</nav>
-
-      {/* navmenu */}
-      <NavigationMenu/>
+			<nav className='navigation-nav'></nav>
 		</div>
 	);
-}
- 
+};
 
 export default Navigation;
