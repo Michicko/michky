9b4b3d696005f26874a04ecb2a5af74d29c9a434
Michael Solomon <michicko81@gmail.com>
2022-11-18 13:21:46 +0100

    fix

--- app.js
+++ app.js
@@ -18,6 +18,29 @@ app.use(
     credentials: true, //access-control-allow-credentials:true
   })
 );
+app.use((req, res, next) => {
+  // Website you wish to allow to connect
+  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+
+  // Request methods you wish to allow
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+
+  // Request headers you wish to allow
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With,content-type,Origin,Accept"
+  );
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader("Access-Control-Allow-Credentials", true);
+
+  // Pass to next layer of middleware
+  next();
+});
 
 // body parser, reading data from the body req.body
 // app.use(cookieParser());
