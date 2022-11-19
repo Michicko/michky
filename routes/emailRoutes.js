657c1e0fd94be0335f811aa0133b2b85ad55c3fa
Michael Solomon <michicko81@gmail.com>
2022-11-19 17:50:30 +0100

    edit cors again

--- routes/emailRoutes.js
+++ routes/emailRoutes.js
@@ -3,27 +3,16 @@ const { sendMail } = require("../controller/emailControllers");
 
 const router = express.Router();
 
+router.route("/").post(sendMail);
 router.use(function (req, res, next) {
-  // res.setHeader("Access-Control-Allow-Origin", "https://michky.vercel.app");
-  // res.setHeader("Access-Control-Allow-Credentials", "true");
-  // res.setHeader("Access-Control-Max-Age", "1800");
-  // res.setHeader("Access-Control-Allow-Methods", "GET,POST,PATCH,PUT,OPTIONS");
-  // res.setHeader(
-  //   "Access-Control-Allow-Headers",
-  //   "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
-  // );
   res.header("Access-Control-Allow-Origin", "https://michky.vercel.app");
   res.header(
-    "Access-Control-Allow-Methods",
-    req.headers["access-control-request-method"]
-  );
-  res.header(
     "Access-Control-Allow-Headers",
-    req.headers["access-control-request-headers"]
+    "Origin, X-Requested-With, Content-Type, Accept"
   );
+  res.header("Access-Control-Allow-Methods", "POST, GET");
   res.header("Access-Control-Max-Age", 60 * 60 * 24 * 365);
   next();
 });
-router.route("/").post(sendMail);
 
 module.exports = router;
