b7a2b45cf9653d41680effbadbd883057cda79e7
Michael Solomon <michicko81@gmail.com>
2022-11-19 12:22:27 +0100

    edit cors

--- app.js
+++ app.js
@@ -31,11 +31,21 @@ const corsOptions = {
   credentials: true, //access-control-allow-credentials:true
   optionSuccessStatus: 200,
 };
+
 app.use(cors(corsOptions));
 
 // body parser, reading data from the body req.body
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 
 // projects routes
 app.use("/api/v1/projects", projectRouter);
--- routes/emailRoutes.js
+++ routes/emailRoutes.js
@@ -4,16 +4,5 @@ const { sendMail } = require("../controller/emailControllers");
 const router = express.Router();
 
 router.route("/").post(sendMail);
-// router.route("/", (req, res, next) => {
-//   res.setHeader("Access-Control-Allow-Origin", "*");
-//   res.setHeader("Access-Control-Allow-Credentials", "true");
-//   res.setHeader("Access-Control-Max-Age", "1800");
-//   res.setHeader("Access-Control-Allow-Headers", "content-type");
-//   res.setHeader(
-//     "Access-Control-Allow-Methods",
-//     "PUT, POST, GET, DELETE, PATCH, OPTIONS"
-//   );
-//   next();
-// });
 
 module.exports = router;
