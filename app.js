3dd66b7cc295b90312537de43b0b468e40ce6566
Michael Solomon <michicko81@gmail.com>
2022-11-20 01:32:10 +0100

    cors

--- app.js
+++ app.js
@@ -6,6 +6,26 @@ const globalError = require("./controller/errorControllers");
 
 const app = express();
 
+app.use(function (req, res, next) {
+  // res.header("Access-Control-Allow-Origin", "*");
+  const allowedOrigins = [
+    "http://localhost:3000",
+    "http://michky.vercel.app",
+    "https://zohomail.com",
+  ];
+  const origin = req.headers.origin;
+  if (allowedOrigins.includes(origin)) {
+    res.setHeader("Access-Control-Allow-Origin", origin);
+  }
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  res.header("Access-Control-Allow-credentials", true);
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, UPDATE");
+  next();
+});
+
 // middlewares
 if (process.env.NODE_ENV === "production") {
   app.use(morgan("dev"));
@@ -49,16 +69,16 @@ app.use(express.urlencoded({ extended: true }));
 //   next();
 // });
 
-app.use(
-  cors({
-    origin: [
-      "http://localhost:3000",
-      "https://michky.vercel.app",
-      "https://zohomail.com",
-    ],
-    credentials: true,
-  })
-);
+// app.use(
+//   cors({
+//     origin: [
+//       "http://localhost:3000",
+//       "https://michky.vercel.app",
+//       "https://zohomail.com",
+//     ],
+//     credentials: true,
+//   })
+// );
 
 // Routers
 const projectRouter = require("./routes/projectRoutes");
