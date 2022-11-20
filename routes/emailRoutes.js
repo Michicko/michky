2255f74c7cc0fa07b9282c8de23a59ed02ac16c7
Michael Solomon <michicko81@gmail.com>
2022-11-20 02:48:35 +0100

    cors

--- routes/emailRoutes.js
+++ routes/emailRoutes.js
@@ -2,31 +2,6 @@ const express = require("express");
 const { sendMail } = require("../controller/emailControllers");
 
 const router = express.Router();
-
-router.use(function (req, res, next) {
-  console.log(req.headers.origin);
-  next();
-});
-
-router.use(function (req, res, next) {
-  // res.header("Access-Control-Allow-Origin", "*");
-  const allowedOrigins = [
-    "http://localhost:3000",
-    "https://michky.vercel.app",
-    "https://zohomail.com",
-  ];
-  const origin = req.headers.origin;
-  if (allowedOrigins.includes(origin)) {
-    res.setHeader("Access-Control-Allow-Origin", origin);
-  }
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-  );
-  res.header("Access-Control-Allow-credentials", true);
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, UPDATE");
-  next();
-});
 router.route("/").post(sendMail);
 
 module.exports = router;
