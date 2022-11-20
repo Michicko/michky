b1a6b6ad2279eb89297a2933f4848b2644e0b0d9
Michael Solomon <michicko81@gmail.com>
2022-11-20 07:13:49 +0100

    cleaned all

--- app.js
+++ app.js
@@ -6,84 +6,15 @@ const globalError = require("./controller/errorControllers");
 
 const app = express();
 
-app.use(function (req, res, next) {
-  // res.header("Access-Control-Allow-Origin", "*");
-  const allowedOrigins = [
-    "http://localhost:3000",
-    "https://michky.vercel.app",
-    "https://zohomail.com",
-  ];
-  const origin = req.headers.origin;
-  if (allowedOrigins.includes(origin)) {
-    res.setHeader("Access-Control-Allow-Origin", origin);
-  }
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-  );
-  res.header("Access-Control-Allow-credentials", true);
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, PATCH");
-  next();
-});
-
-app.use(function (req, res, next) {
-  console.log(req.headers.origin);
-  next();
-});
-
 // middlewares
 if (process.env.NODE_ENV === "production") {
   app.use(morgan("dev"));
 }
-
-// const whitelist = ["http://localhost:3000", "https://michky.vercel.app"];
-
-// cors
-// app.use(
-//   cors({
-//     origin: "*",
-//     credentials: true, //access-control-allow-credentials:true
-//     optionsSuccessStatus: 200,
-//   })
-// );
-
-// const corsOptions = {
-//   origin: "https://michky.vercel.app",
-//   credentials: true, //access-control-allow-credentials:true
-// };
-
-// app.use(cors(corsOptions));
-
 // body parser, reading data from the body req.body
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// app.use(
-//   cors({
-//     origin: "https://michky.vercel.app",
-//     optionsSuccessStatus: 200,
-//     preflightContinue: true,
-//   })
-// );
-
-// app.use(function (req, res, next) {
-//   res.header("Access-Control-Allow-Origin", "https://michky.vercel.app");
-//   res.header("Access-Control-Allow-Credentials", true);
-//   res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
-//   res.header("Access-Control-Allow-Headers", "Origin, Content-Type, Accept");
-//   next();
-// });
-
-// app.use(
-//   cors({
-//     origin: [
-//       "http://localhost:3000",
-//       "https://michky.vercel.app",
-//       "https://zohomail.com",
-//     ],
-//     credentials: true,
-//   })
-// );
+app.use(cors());
 
 // Routers
 const projectRouter = require("./routes/projectRoutes");
@@ -93,6 +24,13 @@ const emailRouter = require("./routes/emailRoutes");
 app.use("/api/v1/projects", projectRouter);
 // email routes
 app.use("/api/v1/contacts", emailRouter);
+
+// render client
+// app.use(express.static(path.join(__dirname, "/client/build")));
+
+// app.get("*", (req, res) => {
+//   res.sendFile(path.join(__dirname, "/client/build", "index.html"));
+// });
 
 // Not found
 app.all("*", (req, res, next) => {
--- client/src/components/Form.js
+++ client/src/components/Form.js
@@ -1,5 +1,4 @@
 import { useRef, useState } from "react";
-import axios from "axios";
 
 const Form = ({ setAlertMessage }) => {
   const btn = useRef(null);
