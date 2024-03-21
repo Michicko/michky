31fd35a435a18b77a5553d389ae043d8d47bb60c
Michael <michicko81@gmail.com>
2024-03-21 18:52:56 +0100

    fixed cors options

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const path = require("path");
 const morgan = require("morgan");
 const cors = require("cors");
 const globalError = require("./controller/errorControllers");
@@ -14,7 +13,33 @@ if (process.env.NODE_ENV === "production") {
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use(cors());
+app.use(function (req, res, next) {
+  // res.setHeader("Access-Control-Allow-Origin", "http://localhost:5173");
+  res.setHeader("Access-Control-Allow-Origin", "https://michky.netlify.app/");
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  res.setHeader("access-control-expose-headers", "set-cookie");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, PATCH, DELETE, HEAD, OPTIONS"
+  );
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type,X-PINGOTHER, Accept"
+  );
+  next();
+});
+
+const corsOptions = {
+  //To allow requests from client
+  // origin: "http://localhost:5173",
+  methods: ["GET", "POST", "PATCH", "DELETE"],
+  origin: "https://michky.netlify.app/",
+  credentials: true,
+  exposedHeaders: ["set-cookie"],
+};
+
+
+app.use(cors(corsOptions));
 
 // Routers
 const projectRouter = require("./routes/projectRoutes");
@@ -25,12 +50,6 @@ app.use("/api/v1/projects", projectRouter);
 // email routes
 app.use("/api/v1/contacts", emailRouter);
 
-// render client
-// app.use(express.static(path.join(__dirname, "/client/build")));
-
-// app.get("*", (req, res) => {
-//   res.sendFile(path.join(__dirname, "/client/build", "index.html"));
-// });
 
 // Not found
 app.all("*", (req, res, next) => {
